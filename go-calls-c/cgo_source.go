f3db6ada5a2f70bbe245b94406c893117511ffcc
J.J. Montgomery <monty@uber.com>
2018-03-16 15:00:54 -0700

    Figured out the problem to get the go-calls-c example to work
    Buck requires the cgo import paths to be relative to the Buck project root, which is wrong. The go toolchain expects the paths to be relative to the folder the files are in.

--- go-calls-c/cgo_source.go
+++ go-calls-c/cgo_source.go
@@ -1,6 +1,8 @@
 package main
 
-// #include "call_from_go.h"
+// this include path is wrong as it should be relative to the current folder, this does not build with the go toolchain
+
+// #include "go-calls-c/call_from_go.h"
 import "C"
 
 func CallC() {
